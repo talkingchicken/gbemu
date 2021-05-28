15f6e89cb702a9f04bed9b25ee41eb64f1d604bb
talking_chicken <talkingchicken@users.noreply.github.com>
2021-05-27 17:22:43 -0700

    initial empty commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace gbemu
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new GbEmu())
+                game.Run();
+        }
+    }
+}
